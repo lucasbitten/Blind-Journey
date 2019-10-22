1adb0a1addfcb22306b4ab15fef39d1751615f59
Lucas <luksbitten@gmail.com>
2019-10-21 21:03:50 -0400

    Player animations and platforms behaviours added

---
+++ Blind Journey/Assets/Scripts/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+    }
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "FootCollider"){
+            spriteRenderer.enabled = true;
+        }
+    }
+
+}
---
+++ Blind Journey/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+
+    [SerializeField]
+    float speed = 2;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+        if (rb.velocity.x > 0){
+            transform.localScale = Vector3.one;
+        } else if (rb.velocity.x < -0.1f){
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        if(Input.GetKeyUp(KeyCode.D)){
+            rb.velocity = Vector2.zero;
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        if(Input.GetKeyUp(KeyCode.A)){
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
